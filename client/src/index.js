6055920aa5e98097571dbcc28bdf001070be3234
saharZ95 <saharonaz95@gmail.com>
2023-02-14 13:11:07 +0200

    creating light and dark mode

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import globalRedducer from "state";
+import { Provider } from "react-redux";
+
+const store = configureStore({
+  reducer: {
+    global: globalRedducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
