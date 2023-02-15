5e4ae10a185711523a8b143e961e215f608c149d
saharZ95 <saharonaz95@gmail.com>
2023-02-15 18:33:45 +0200

    add a few changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,35 @@
-rafce;
-import React from "react";
+import React, { useState } from "react";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  Menu as MenuIcon,
+  Search,
+  SettingsOutlined,
+  ArrowDropOutLined,
+} from "@mui/icons-material";
+import FlexBetween from "components/FlexBetween";
+import { useDispatch, useDispatch } from "react-redux";
+import { setmode } from "state";
+import { useState } from "react";
+// import profileImage from "assets/profile.jpeg";
+import { AppBar, Toolbar, useTheme } from "@mui/material";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const useDispatch = useDispatch();
+  const theme = useTheme();
+  return (
+    <AppBar
+      sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+      }}
+    >
+      <Toolbar sx={{ justifycontent: "space-between" }}>
+        {/* LEFT SIDE */}
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
